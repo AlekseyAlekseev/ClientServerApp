52cdb01d1077ee2836ac02746338b5c7f1a5dbbb
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-03-27 19:27:40 +0300

    last commit

--- src/main/java/ru/netology/Client/Client.java
+++ src/main/java/ru/netology/Client/Client.java
@@ -1,57 +1,43 @@
 package ru.netology.Client;
 
+import ru.netology.MyClosable;
 import ru.netology.Server.Server;
 
 import java.io.*;
 import java.net.Socket;
 
-public class Client {
-
-    private static Socket clientSocket; //сокет для общения
-    private static BufferedReader reader; // нам нужен ридер читающий с консоли, иначе как
-    // мы узнаем что хочет сказать клиент?
-    private static BufferedReader in; // поток чтения из сокета
-    private static PrintWriter out; // поток записи в сокет
-
+public class Client implements AutoCloseable {
 
     public static void main(String[] args) {
 
-        try {
-            try {
-                // адрес - локальный хост, порт - 4004, такой же как у сервера
-                clientSocket = new Socket("localhost", Server.SERVER_PORT);
+        try (// адрес - локальный хост, порт - 7777, такой же как у сервера
+             Socket clientSocket = new Socket("localhost", Server.SERVER_PORT);
 
-                //  у сервера доступ на соединение
-                reader = new BufferedReader(new InputStreamReader(System.in));
+             //  у сервера доступ на соединение
+             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-                // читать соообщения с сервера
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                // писать туда же
-                out = new PrintWriter(clientSocket.getOutputStream(), true); // отправлять
+             // читать соообщения с сервера
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             // писать туда же
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) // отправлять
+        {
+            System.out.println("Введите сообщение для сервера:");
+            // если соединение произошло и потоки успешно созданы - мы можем
+            //  работать дальше и предложить клиенту что то ввести
+            // если нет - вылетит исключение
 
-
-                System.out.println("Введите сообщение для сервера:");
-                // если соединение произошло и потоки успешно созданы - мы можем
-                //  работать дальше и предложить клиенту что то ввести
-                // если нет - вылетит исключение
-
-                String message = reader.readLine(); // ждём пока клиент что-нибудь
-
-                // не напишет в консоль
-                out.write(message + "\n"); // отправляем сообщение на сервер
-                out.flush(); // выталкиваем все из буфера
-
-                String serverMessage = in.readLine(); // ждём, что скажет сервер
-                System.out.println(serverMessage); // получив - выводим на экран
-
-            } finally {
-                System.out.println("Клиент был закрыт...");
-                clientSocket.close();
-                in.close();
-                out.close();
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            String message = reader.readLine(); // ждём пока клиент что-нибудь напишет в консоль
+            out.write(message + "\n"); // отправляем сообщение на сервер
+            out.flush(); // выталкиваем все из буфера
+            String serverMessage = in.readLine(); // ждём, что скажет сервер
+            System.out.println(serverMessage); // получив - выводим на экран
+        } catch (Exception e) {
+            System.out.println(e);
         }
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Клиент выключен");
     }
 }
--- src/main/java/ru/netology/Server/Server.java
+++ src/main/java/ru/netology/Server/Server.java
@@ -1,58 +1,39 @@
 package ru.netology.Server;
 
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class Server implements AutoCloseable {
 
-    private static Socket clientSocket; //сокет для общения
-    private static ServerSocket server; // серверсокет
-    private static BufferedReader in; // поток чтения из сокета
-    private static PrintWriter out; // поток записи в сокет
     public static final int SERVER_PORT = 7777;
 
     public static void main(String[] args) {
 
-        try {
-            try {
-                server = new ServerSocket(SERVER_PORT); // серверсокет прослушивает порт 7777
-
-                System.out.println("Сервер запущен!"); // хорошо бы серверу
-                //   объявить о своем запуске
-
-                clientSocket = server.accept(); // accept() будет ждать пока
-                //кто-нибудь не захочет подключиться
-
-                try {
-                    // установив связь и воссоздав сокет для общения с клиентом можно перейти
-                    // к созданию потоков ввода/вывода.
-                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // принимать
-                    out = new PrintWriter(clientSocket.getOutputStream(), true); // отправлять
-
-                    final String message = in.readLine(); // ждём пока клиент что-нибудь нам напишет
-                    System.out.println(message);
-
-                    out.write("Привет, это Сервер! Подтверждаю, вы написали : " + message + "\n"); // не долго думая отвечает клиенту
-                    out.flush(); // выталкиваем все из буфера
-
-                } finally {
-                    // в любом случае сокет будет закрыт
-                    clientSocket.close();
-                    // потоки тоже хорошо бы закрыть
-                    in.close();
-                    out.close();
-                }
-            } finally {
-                System.out.println("Сервер закрыт");
-                server.close();
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        System.out.println("Сервер запущен");
+        try (ServerSocket server = new ServerSocket(SERVER_PORT); // серверсокет прослушивает порт 7777
+             Socket clientSocket = server.accept(); // ожидаем подключение клиента
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // принимаем
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) // отправляем
+        {
+            final String message = in.readLine(); // ждём пока клиент что-нибудь нам напишет
+            System.out.println(message);
+            out.write("Привет, это Сервер! Подтверждаю, вы написали : " + message + "\n"); // не долго думая отвечает клиенту
+            out.flush(); // выталкиваем все из буфера
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Сервер выключен");
+    }
 }
+
+
+
 
